f926117ea3c912ec1028ede4c221cb590eebdaa8
Wei Yang <wei@weizilla.com>
2015-12-11 22:11:32 -0600

    change api for creating workouts

--- workout-logger-lib/src/main/java/com/weizilla/workout/logger/entity/Workout.java
+++ workout-logger-lib/src/main/java/com/weizilla/workout/logger/entity/Workout.java
@@ -6,30 +6,42 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
+import java.util.UUID;
 
 public class Workout
 {
+    private final UUID uuid;
     private final String type;
     private final Duration duration;
     private final LocalDate date;
     private final Instant entryTime;
 
-    public Workout(String type, Duration duration, LocalDate date)
-    {
-        this(type, duration, date, Instant.now());
-    }
-
     @JsonCreator
     public Workout(
         @JsonProperty("type") String type,
         @JsonProperty("duration") Duration duration,
-        @JsonProperty("date") LocalDate date,
-        @JsonProperty("entryTime") Instant entryTime)
+        @JsonProperty("date") LocalDate date)
     {
+        this(UUID.randomUUID(), type, duration, date, Instant.now());
+    }
+
+    public Workout(
+        UUID uuid,
+        String type,
+        Duration duration,
+        LocalDate date,
+        Instant entryTime)
+    {
+        this.uuid = uuid;
         this.type = type;
         this.duration = duration;
         this.date = date;
         this.entryTime = entryTime;
+    }
+
+    public UUID getUuid()
+    {
+        return uuid;
     }
 
     public String getType()
--- workout-logger-lib/src/test/java/com/weizilla/workout/logger/json/ObjectMappersTest.java
+++ workout-logger-lib/src/test/java/com/weizilla/workout/logger/json/ObjectMappersTest.java
@@ -10,37 +10,45 @@ import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.UUID;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
 
 public class ObjectMappersTest
 {
-    private Workout workout;
-    private String json;
+    private LocalDate date;
+    private Duration duration;
+    private String type;
 
     @Before
     public void setUp() throws Exception
     {
-        LocalDate date = LocalDate.of(2015, Month.NOVEMBER, 21);
-        workout = new Workout("TYPE", Duration.ofHours(1), date, Instant.ofEpochSecond(1448146540));
-        json = TestUtils.readFile("workout-json-test.json");
+        date = LocalDate.of(2015, Month.NOVEMBER, 21);
+        duration = Duration.ofHours(1);
+        type = "TYPE";
     }
 
     @Test
     public void serializesWorkout() throws Exception
     {
+        UUID uuid = UUID.fromString("e487cc32-c5d9-417a-b0df-9aa0eb9154c3");
+        Workout workout = new Workout(uuid, type, duration, date, Instant.ofEpochSecond(1448146540));
+        String expected = TestUtils.readFile("workout-serialize-test.json");
         String actual = ObjectMappers.OBJECT_MAPPER.writeValueAsString(workout);
-        JSONAssert.assertEquals(json, actual, true);
+        JSONAssert.assertEquals(expected, actual, true);
     }
 
     @Test
     public void deserializeWorkout() throws Exception
     {
-        Workout actual = ObjectMappers.OBJECT_MAPPER.readValue(json, Workout.class);
-        assertThat(actual.getType(), is(workout.getType()));
-        assertThat(actual.getDuration(), is(workout.getDuration()));
-        assertThat(actual.getDate(), is(workout.getDate()));
-        assertThat(actual.getEntryTime(), is(workout.getEntryTime()));
+        String input = TestUtils.readFile("workout-deserialize-test.json");
+        Workout actual = ObjectMappers.OBJECT_MAPPER.readValue(input, Workout.class);
+        assertThat(actual.getUuid(), is(notNullValue()));
+        assertThat(actual.getType(), is(type));
+        assertThat(actual.getDuration(), is(duration));
+        assertThat(actual.getDate(), is(date));
+        assertThat(actual.getEntryTime(), is(notNullValue()));
     }
 }
