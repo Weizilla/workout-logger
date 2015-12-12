5cbd99256694776e2a28b6699a05aeb4ec118006
Wei Yang <wei@weizilla.com>
2015-12-11 21:51:56 -0600

    move web app main class so intellij can pick up all beans

--- workout-logger-web-app/src/main/java/com/weizilla/workout/logger/WebApplication.java
+++ workout-logger-web-app/src/main/java/com/weizilla/workout/logger/WebApplication.java
@@ -1,12 +1,9 @@
-package com.weizilla.workout.logger.web;
+package com.weizilla.workout.logger;
 
-import com.weizilla.workout.logger.WorkoutLogger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = WorkoutLogger.class)
 public class WebApplication
 {
     public static void main(String[] args) throws Exception
