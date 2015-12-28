8872549395d0c6dd75344eca9321af00056ef0ad
Wei Yang <wei@weizilla.com>
2015-12-27 17:13:30 -0800

    add cross origin resource sharing

--- workout-logger-web-app/src/main/java/com/weizilla/workout/logger/WebApplication.java
+++ workout-logger-web-app/src/main/java/com/weizilla/workout/logger/WebApplication.java
@@ -2,10 +2,19 @@ package com.weizilla.workout.logger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class WebApplication
+public class WebApplication extends WebMvcConfigurerAdapter
 {
+    @Override
+    public void addCorsMappings(CorsRegistry registry)
+    {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:9000");
+    }
+
     public static void main(String[] args) throws Exception
     {
         SpringApplication.run(WebApplication.class, args);
