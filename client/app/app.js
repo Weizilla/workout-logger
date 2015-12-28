45489d612c0888842675d260f4ee4de7a5634da3
Wei Yang <wei@weizilla.com>
2015-12-27 19:52:02 -0800

    add refresh button and bootstrap

--- client/app/app.js
+++ client/app/app.js
@@ -1,5 +1,6 @@
 import angular from "angular";
 import * as WorkoutsModule from "./workouts";
+import "bootstrap/css/bootstrap.min.css!";
 
 angular.module("workoutLoggerApp", [])
     .factory("WorkoutsService", WorkoutsModule.service)
