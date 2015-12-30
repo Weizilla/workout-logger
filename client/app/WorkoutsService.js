afdc1f6d74e0683d6f96bff39f468c712c7102e8
Wei Yang <wei@weizilla.com>
2015-12-29 22:08:22 -0800

    run on port 80

--- client/app/WorkoutsService.js
+++ client/app/WorkoutsService.js
@@ -6,11 +6,11 @@ class WorkoutsService {
     }
 
     getWorkouts() {
-        return this.$http.get("http://localhost:8080/api/workouts").then(r => r.data);
+        return this.$http.get("api/workouts").then(r => r.data);
     }
 
     addWorkout(workout) {
-        return this.$http.post("http://localhost:8080/api/workouts", workout).then(r => r.data);
+        return this.$http.post("api/workouts", workout).then(r => r.data);
     }
 
     static factory($http) {
